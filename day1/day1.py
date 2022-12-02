9b384e191d660a87dcf70c8dc3a3e8af531dbf87
Vallfors99 <axel.vallfors@gmail.com>
2022-12-02 15:08:36 +0100

    day 1 and 2

---
+++ day1/day1.py
@@ -0,0 +1,25 @@
+# input parsing
+input_file = 'input.txt'
+with open(input_file) as infile:
+    lines = [line.strip('\n') for line in infile]
+
+# part 1
+calories_by_elf = {}
+elf_id = 0
+calory_sum = 0
+for line in lines:
+    if line == '':
+        elf_id +=1
+        calories_by_elf[elf_id] = calory_sum
+        calory_sum = 0
+    else:
+        calory_sum += int(line)
+if calory_sum !=0:
+    elf_id +=1
+    calories_by_elf[elf_id] = calory_sum
+print(max(calories_by_elf.values()))
+
+#part 2
+inventory_totals = list(calories_by_elf.values())
+inventory_totals.sort(reverse=True)
+print(sum(inventory_totals[0:3]))
---
+++ day2/day2.py
@@ -0,0 +1,51 @@
+input_file = 'input.txt'
+with open(input_file) as infile:
+    rounds = [line.strip('\n') for line in infile]
+    rounds = [play.split(' ') for play in rounds]
+
+# play games
+# X = Rock, Y = Paper, Z = Scissors
+# A = Rock, B  = Paper, C = Scissors
+value_of_play = {'A': 1, 'B':2, 'C':3, 'X':1, 'Y':2,'Z':3}
+wins_player = [['A','Y'],['B','Z'],['C','X']]
+
+opponent_scores = []
+player_scores = []
+for round in rounds:
+    if value_of_play[round[0]] == value_of_play[round[1]]: #draw
+        opponent_scores.append(3 + value_of_play[round[0]])
+        player_scores.append(3 + value_of_play[round[1]])
+
+    elif round in wins_player: #player won
+        opponent_scores.append(0 + value_of_play[round[0]])
+        player_scores.append(6 + value_of_play[round[1]])
+
+    else: # opponent won
+        opponent_scores.append(6 + value_of_play[round[0]])
+        player_scores.append(0 + value_of_play[round[1]])
+    
+
+
+print(sum(player_scores))
+
+
+# part 2
+# ta nästa element i ordningen
+# framåt = win
+# bakåt = loss
+list_plays = ['A','B','C']
+score_total = 0
+for round in rounds:
+    idx_opponent_play  = list_plays.index(round[0])
+    if round[1] == 'X': # loose
+        score_round = 0
+        player_play = list_plays[(idx_opponent_play - 1) % len(list_plays)] # next value in order
+    elif round[1] == 'Y': # tie
+        player_play = round[0]
+        score_round = 3
+    else: 
+        player_play = list_plays[(idx_opponent_play + 1) % len(list_plays)] # previous value in order
+        score_round = 6
+    score_total += score_round + value_of_play[player_play]
+print(score_total)
+

f2f4e89d5576fe6adf9fc88e68e98f0be46ee95f
Vallfors99 <axel.vallfors@gmail.com>
2022-12-06 19:32:07 +0100

    easy peasy

--- day6/part_1.py
+++ day6/part_1.py
@@ -2,3 +2,12 @@
 input_file = 'day6\input.txt'
 with open(input_file) as infile:
     lines = [line.strip() for line in infile]
+
+
+result = None
+message = [c for c in lines[0]]
+for i in range(len(message)):
+    if len(set(message[i:i+4])) == 14:
+        result = i+4
+        break
+print(result)

ef48cb5b7d6a5b7bc5eaa1722fac4bf5b4fd8244
Vallfors99 <78348794+Vallfors99@users.noreply.github.com>
2022-12-08 18:30:43 +0100

    Update part_1.py

--- day8/part_1.py
+++ day8/part_1.py
@@ -1,4 +1,34 @@
-# input parsing
-input_file = 'day8\input.txt'
-with open(input_file) as infile:
-    lines = [line.strip() for line in infile]
+import numpy as np
+with open("input.txt") as infile:
+    lines = [[int(elem) for elem in line.strip()] for line in infile]
+
+grid = np.array(lines)
+grid_visible = np.empty(np.shape(grid),dtype=str)
+print(grid)
+trees_outside = []
+print(len(grid))
+border_count = 0
+for row in range(len(grid)):
+    for col in range(len(grid[0])):
+        tree_height = grid[row][col]
+        on_border = row == 0 or col == 0 or row == len(grid)-1 or col == len(grid[0])-1 
+
+        if on_border:
+            trees_outside.append((row,col))
+            grid_visible[row,col] = str(tree_height)
+            border_count += 1
+            print(f'Border! {border_count}, {(row,col)}')
+
+        else:
+            # check if visible
+            left_row = grid[row][0:col][::-1]
+            right_row = grid[row][col+1:len(grid[0])]
+            up_row = grid[:,col][0:row][::-1]
+            down_row = grid[:,col][row+1:len(grid)]
+
+            if tree_height > max(left_row) or tree_height > max(right_row) or tree_height > max(up_row) or tree_height > max(down_row):
+                trees_outside.append((row,col))
+                grid_visible[row,col] = str(tree_height)
+            
+print(len(trees_outside))
+print(grid_visible)

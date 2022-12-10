56d05b7fb041e0e00d93498371576d11c186e74c
Vallfors99 <78348794+Vallfors99@users.noreply.github.com>
2022-12-10 09:58:25 +0100

    Update part_1.py

--- day10/part_1.py
+++ day10/part_1.py
@@ -1,4 +1,17 @@
-# input parsing
-input_file = 'day10\input.txt'
+input_file = "input.txt"
 with open(input_file) as infile:
     lines = [line.strip() for line in infile]
+lines = [l.split(' ') for l in lines]
+register_val = [1]
+for command_val in lines:
+    if len(command_val) ==1 and command_val[0] == "noop": #1 cycle, no change in register val
+        register_val.append(register_val[-1])
+
+    else: # two cycles
+        command,val = command_val
+        val = int(val)
+        register_val.append(register_val[-1])
+        register_val.append(register_val[-1]+val)
+
+signal_strengths = [(i+1)*register_val[i] for i in range(19,249,40)]
+print(sum(signal_strengths))

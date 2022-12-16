1a6e090a5fb036d68833475961d6115a1b2b3d49
Vallfors99 <axel.vallfors@gmail.com>
2022-12-16 11:07:33 +0100

    wip

--- day17/part_1.py
+++ day17/part_1.py
@@ -2,3 +2,9 @@
 input_file = 'day17\input.txt'
 with open(input_file) as infile:
     lines = [line.strip() for line in infile]
+
+lines = [line.replace('Valve ','').replace(' has flow rate=',',').split(';') for line in lines]
+lines = [[line[0].split(','),line[1].split(',')] for line in lines]
+for idx in range(len(lines)):
+    lines[idx][1][0] = lines[idx][1][0].split(' ')[5]
+    print(lines[idx])

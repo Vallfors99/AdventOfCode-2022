43fca6ebaf164766cd9dbacef8e502eff37ed1a5
Vallfors99 <axel.vallfors@gmail.com>
2022-12-17 13:48:37 +0100

    wip

--- day17/part_1.py
+++ day17/part_1.py
@@ -1,33 +1,63 @@
 # imports:
 from itertools import groupby
 import collections
-
+import copy
 # functions:
 def drop_rock(grid,rocks_queue:collections.deque,jet_stream_queue: collections.deque):
     '''drop a single rock into the grid'''
     has_landed = False
     # add empty rows
-    idx_current_rocks_height = len(grid)-1
-    for i in range(3):
+    
+    while grid[-4] != ['.','.','.','.','.','.','.']: 
         grid.append(['.','.','.','.','.','.','.'])
     # spawn rock positions
     rock_positions = rocks_queue[0]
     rock_positions = [ [len(grid)-1-coords[0], coords[1]]for coords in rock_positions ]    
-    print(rock_positions)
     
+
     while not has_landed:
-        old_rock_positions = rock_positions
+        # save old rock positions
+        old_rock_positions = copy.deepcopy(rock_positions)
+
         # get jet stream direction
-        jet_dir = 1 if [jet_stream_queue[0]]=='>' else -1
-        jet_stream_queue.rotate()
-        # get new coordinates after jet push
+        jet_dir = 1 if jet_stream_queue[0] =='>' else -1
+
+        # Perform jet push:
         for i in range(len(rock_positions)):
-            rock_positions[i][1] + jet_dir
+            new_pos = rock_positions[i]
+            new_pos[1] += jet_dir
+            if new_pos[1] == 7 or new_pos[1] == -1 or grid[new_pos[0]][new_pos[1]] == '#':
+                # no jet push, restore old positions and break
+                rock_positions = copy.deepcopy(old_rock_positions)
+                break
+            # update rock positions for the current rock segment
+            rock_positions[i] = new_pos
+        
+        # rotate jet stream queue 
+        jet_stream_queue.rotate(-1)
+            
+        # save old rock positions as positions after jet push
+        old_rock_positions = copy.deepcopy(rock_positions)
+
         # let rock fall
-
-
-
-    return grid, current_height
+        for i in range(len(rock_positions)):
+            new_pos = rock_positions[i]
+            new_pos[0] -= 1
+            if grid[new_pos[0]][new_pos[1]] == "#" or grid[new_pos[0]][new_pos[1]] == '-':
+                # no fall, rock has landed and the fall should be cancelled
+                rock_positions = copy.deepcopy(old_rock_positions)
+                has_landed = True
+                break
+            # update rock positions for the current rock segment
+            rock_positions[i] = new_pos
+        
+    # modify grid
+    for rock_position in rock_positions:
+        grid[rock_position[0]][rock_position[1]] ='#'
+    
+    # update rocks queue
+    rocks_queue.rotate(-1)
+    return grid,rocks_queue,jet_stream_queue
 
 # input parsing:
 input_file = 'day17\input.txt'
@@ -55,10 +85,13 @@ rocks_queue = collections.deque(rock_shapes_coords)
 
 #define initial grid
 grid = [['-' for col in range(7)]]
+for i in range(3):
+    grid.append(['.','.','.','.','.','.','.'])
 
 # define loop for rocks falling
 for iter in range(10):
-    grid,current_height = drop_rock(grid,rocks_queue,jet_stream_queue)
-
-# for row_idx in reversed(range(len(grid))):
-#     print("".join(grid[row_idx]))
+    grid,rocks_queue,jet_stream_queue = drop_rock(grid,rocks_queue,jet_stream_queue)
+    print('')
+    print('')
+    for row_idx in reversed(range(len(grid))):
+        print("".join(grid[row_idx]))

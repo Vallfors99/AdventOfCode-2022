5b85331ff51ea0567e9a316eafe062f201c41188
Vallfors99 <axel.vallfors@gmail.com>
2022-12-18 19:07:35 +0100

    wip part 2

--- day18/part_1.py
+++ day18/part_1.py
@@ -1,4 +1,20 @@
 # input parsing
 input_file = 'day18\input.txt'
 with open(input_file) as infile:
-    lines = [line.strip() for line in infile]
+    cubes = [[int(elem) for elem in line.strip().split(',')] for line in infile]
+print(cubes)
+
+area_count = 0
+for cube in cubes:
+    cube_up = [cube[0],cube[1],cube[2]+1]
+    cube_down = [cube[0],cube[1],cube[2]-1]
+    cube_x_right = [cube[0]+1,cube[1],cube[2]]
+    cube_x_left = [cube[0]-1,cube[1],cube[2]]
+    cube_y_right = [cube[0],cube[1]+1,cube[2]]
+    cube_y_left = [cube[0],cube[1]-1,cube[2]]
+    adjacent_cubes = [cube_up,cube_down,cube_x_left,cube_x_right,cube_y_left,cube_y_right]
+
+    for adj_cube in adjacent_cubes:
+        if adj_cube not in cubes:
+            area_count += 1
+print(area_count)

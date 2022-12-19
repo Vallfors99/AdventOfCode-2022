2766a8ff31d423b0c890a74a061fea68a088c5a4
Vallfors99 <axel.vallfors@gmail.com>
2022-12-19 17:19:40 +0100

    cleanup

--- day18/part_2.py
+++ day18/part_2.py
@@ -1,27 +1,20 @@
 import cProfile
 
-def elementwise_addition(list_1,list_2):
-    '''perform elementwise addition on two lists'''
-    for i in range(len(list_1)):
-        list_2[i] += list_1[i]
-    return list_2
-
 
 def get_cube_neighbors(cube,other_cubes):
     '''get coordinates for neighbors of cube, if neighbor exists in other_cubes'''
     neighbors = []
-    neighbor_1 = elementwise_addition(cube,[0,0,1])
-    neighbor_2 = elementwise_addition(cube,[0,0,-1])
-    neighbor_3 = elementwise_addition(cube,[0,1,0])
-    neighbor_4 = elementwise_addition(cube,[0,-1,0])
-    neighbor_5 = elementwise_addition(cube,[1,0,0])
-    neighbor_6 = elementwise_addition(cube,[-1,0,0])
+    neighbor_1 = (cube[0],cube[1],cube[2]+1)
+    neighbor_2 = (cube[0],cube[1],cube[2]-1)
+    neighbor_3 = (cube[0],cube[1]+1,cube[2])
+    neighbor_4 = (cube[0],cube[1]-1,cube[2])
+    neighbor_5 = (cube[0]+1,cube[1],cube[2])
+    neighbor_6 = (cube[0]-1,cube[1],cube[2])
 
     for neighbor in [neighbor_1,neighbor_2,neighbor_3,neighbor_4,neighbor_5,neighbor_6]:
-        if tuple(neighbor) in other_cubes:
-            neighbors.append(tuple(neighbor))
+        if neighbor in other_cubes:
+            neighbors.append(neighbor)
     return neighbors
-
 
 def count_external_surfaces(external_air_cubes,cubes):
     '''count how many external surfaces the cube structure has'''
@@ -37,12 +30,13 @@ def get_all_air_cubes_in_cube_space(cubes):
     min_y = min([elem[1] for elem in cubes])
     max_z = max([elem[2] for elem in cubes])
     min_z = min([elem[2] for elem in cubes])
-    air_cubes = []
+    
+    air_cubes = {}
     for x in range(min_x-1,max_x+2,1):
         for y in range(min_y-1,max_y+2,1):
             for z in range(min_z-1,max_z+2,1):
                 if (x,y,z) not in cubes:
-                    air_cubes.append((x,y,z))
+                    air_cubes[(x,y,z)] = True
     return air_cubes,min_x,max_x,min_y,max_y,min_z,max_z
 
 def find_all_connected_cubes(start_cube,all_cubes):
@@ -79,7 +73,8 @@ def main():
     input_file = 'day18\input.txt'
     with open(input_file) as infile:
         cubes = [[int(elem) for elem in line.strip().split(',')] for line in infile]
-        cubes = [(c[0],c[1],c[2]) for c in cubes]
+    cubes = [(c[0],c[1],c[2]) for c in cubes]
+    cubes = {cube:True for cube in cubes}
 
     # get all air cubes in the cube space (including a padding of 1)
     air_cubes,min_x,max_x,min_y,max_y,min_z,max_z = get_all_air_cubes_in_cube_space(cubes)
@@ -88,11 +83,11 @@ def main():
     edge__air_cube = (min_x-1,min_y-1,min_z-1)
 
     # find all connected air cubes to this air cube
-    air_cubes = {air_cube: True for air_cube in air_cubes}
     external_air_cubes = find_all_connected_cubes(edge__air_cube,air_cubes)
     
     # count the number of external surfaces
     n_external_surfaces = count_external_surfaces(external_air_cubes,cubes)
-    
+
     print(n_external_surfaces)
+
 cProfile.run('main()',sort='cumtime')

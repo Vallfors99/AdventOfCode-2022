cac4fb4ff428ebddf53cfc065b76b4f4f7dc4c62
Vallfors99 <axel.vallfors@gmail.com>
2022-12-19 16:27:34 +0100

    cleanup

--- day18/part_2.py
+++ day18/part_2.py
@@ -90,17 +90,7 @@ def count_external_surfaces_2(external_air_cubes,cubes):
         n_external_surfaces += len(get_cube_neighbors(cube,external_air_cubes))
     return n_external_surfaces
 
-def main():
-    # input parsing
-    input_file = 'day18\input.txt'
-    with open(input_file) as infile:
-        cubes = [[int(elem) for elem in line.strip().split(',')] for line in infile]
-        cubes = [(c[0],c[1],c[2]) for c in cubes]
-
-    area_count = 0
-
-
-    # build a big air cube that contains all lava cubes
+def get_all_air_cubes_in_cube_space(cubes):
     max_x = max([elem[0] for elem in cubes])
     min_x = min([elem[0] for elem in cubes])
     max_y = max([elem[1] for elem in cubes])
@@ -113,19 +103,23 @@ def main():
             for z in range(min_z-1,max_z+2,1):
                 if (x,y,z) not in cubes:
                     air_cubes.append((x,y,z))
+    return air_cubes,min_x,max_x,min_y,max_y,min_z,max_z
 
-
-    # identify all external air cubes, i.e. air cubes that are connected to the outermost point
-    edge_cube = (min_x-1,min_y-1,min_z-1)
-    neighbors = {edge_cube: True}
-    new_neighbors_this_iteration = [edge_cube]
+def find_all_connected_cubes(start_cube,all_cubes):
+    '''
+    Takes a start_cube and a list containing all_cubes in the cube space.
+    Returns a list of all cubes that are connected to the start_cube
+    either directly or indirectly through other cubes in all_cubes
+    '''
+    neighbors = {start_cube: True}
+    new_neighbors_this_iteration = [start_cube]
 
     while True:
         new_neighbors_previous_iteration = new_neighbors_this_iteration
         new_neighbors_this_iteration = []
 
         for neighbor in new_neighbors_previous_iteration:
-            new_neighbors_this_neighbor = get_cube_neighbors(neighbor,air_cubes)
+            new_neighbors_this_neighbor = get_cube_neighbors(neighbor,all_cubes)
             for new_neighbor in new_neighbors_this_neighbor:
                 if not new_neighbor in neighbors:
                     new_neighbors_this_iteration.append(new_neighbor)
@@ -137,8 +131,26 @@ def main():
             for new_neighbor in new_neighbors_this_iteration:
                 neighbors[new_neighbor] = True
 
-    # count visible surfaces by iterating over each external air cube and checking which cube surfaces it can see
-    external_air_cubes = list(neighbors.keys())
-    n_external_surfaces = count_external_surfaces_2(external_air_cubes,cubes)
+    connected_cubes = list(neighbors.keys())
+    return connected_cubes
+
+def main():
+    # input parsing
+    input_file = 'day18\input.txt'
+    with open(input_file) as infile:
+        cubes = [[int(elem) for elem in line.strip().split(',')] for line in infile]
+        cubes = [(c[0],c[1],c[2]) for c in cubes]
+
+    # get all air cubes in the cube space (including a padding of 1)
+    air_cubes,min_x,max_x,min_y,max_y,min_z,max_z = get_all_air_cubes_in_cube_space(cubes)
+
+    # define an edge air cube which is known to be outside of the cube structure
+    edge__air_cube = (min_x-1,min_y-1,min_z-1)
+
+    # find all connected air cubes to this air cube
+    external_air_cubes = find_all_connected_cubes(edge__air_cube,air_cubes)
     
-cProfile.run('main()',sort='tottime')
+    # count the number of external surfaces
+    n_external_surfaces = count_external_surfaces_2(external_air_cubes,cubes)
+    print(n_external_surfaces)
+cProfile.run('main()',sort='cumtime')

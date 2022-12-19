c2838329cd4d5c9ceb58fb63ce98b7c51a02af5c
Vallfors99 <axel.vallfors@gmail.com>
2022-12-19 17:44:03 +0100

    more cleanup

--- day18/part_2.py
+++ day18/part_2.py
@@ -1,29 +1,42 @@
 import cProfile
 
-
 def get_cube_neighbors(cube,other_cubes):
-    '''get coordinates for neighbors of cube, if neighbor exists in other_cubes'''
+    '''
+    get coordinates for any neighbors of cube (x,y,z) present in other_cubes
+    '''
     neighbors = []
-    neighbor_1 = (cube[0],cube[1],cube[2]+1)
-    neighbor_2 = (cube[0],cube[1],cube[2]-1)
-    neighbor_3 = (cube[0],cube[1]+1,cube[2])
-    neighbor_4 = (cube[0],cube[1]-1,cube[2])
-    neighbor_5 = (cube[0]+1,cube[1],cube[2])
-    neighbor_6 = (cube[0]-1,cube[1],cube[2])
+    nb_1 = (cube[0],cube[1],cube[2]+1)
+    nb_2 = (cube[0],cube[1],cube[2]-1)
+    nb_3 = (cube[0],cube[1]+1,cube[2])
+    nb_4 = (cube[0],cube[1]-1,cube[2])
+    nb_5 = (cube[0]+1,cube[1],cube[2])
+    nb_6 = (cube[0]-1,cube[1],cube[2])
 
-    for neighbor in [neighbor_1,neighbor_2,neighbor_3,neighbor_4,neighbor_5,neighbor_6]:
+    for neighbor in nb_1,nb_2,nb_3,nb_4,nb_5,nb_6:
         if neighbor in other_cubes:
             neighbors.append(neighbor)
     return neighbors
 
 def count_external_surfaces(external_air_cubes,cubes):
-    '''count how many external surfaces the cube structure has'''
+    '''
+    takes the following arguments:
+    - external_air_cubes: iterable containing (x,y,z) coordinates of air cubes
+    - cubes: iterable containing (x,y,z) coordinates of cubes
+
+    Returns:
+    - an integer representing the number of cube surfaces facing a cube in external_air_cubes
+    '''
     n_external_surfaces = 0
     for cube in cubes:
         n_external_surfaces += len(get_cube_neighbors(cube,external_air_cubes))
     return n_external_surfaces
 
 def get_all_air_cubes_in_cube_space(cubes):
+    '''
+    Takes a list of positions (x,y,z) for 1x1 cubes in a cube structure.
+    Returns a dict containing positions (x,y,z) for all the air cubes present inside 
+    or around the cube structure
+    '''
     max_x = max([elem[0] for elem in cubes])
     min_x = min([elem[0] for elem in cubes])
     max_y = max([elem[1] for elem in cubes])
@@ -46,20 +59,18 @@ def find_all_connected_cubes(start_cube,all_cubes):
     either directly or indirectly through other cubes in all_cubes
     '''
     neighbors = {start_cube: True}
-    new_neighbors_this_iteration = [start_cube]
+    new_neighbors_this_iteration = {start_cube:True}
 
     while True:
-        print(len(new_neighbors_this_iteration))
         new_neighbors_previous_iteration = new_neighbors_this_iteration
-        new_neighbors_this_iteration = []
+        new_neighbors_this_iteration = dict()
 
         for neighbor in new_neighbors_previous_iteration:
             new_neighbors_this_neighbor = get_cube_neighbors(neighbor,all_cubes)
             for new_neighbor in new_neighbors_this_neighbor:
-                if not new_neighbor in neighbors:
-                    new_neighbors_this_iteration.append(new_neighbor)
+                if not new_neighbor in neighbors and new_neighbor not in new_neighbors_this_iteration:
+                    new_neighbors_this_iteration[new_neighbor] = True
         
-        new_neighbors_this_iteration = set(new_neighbors_this_iteration)
         if len(new_neighbors_this_iteration) == 0:
             break
         else:
@@ -67,6 +78,7 @@ def find_all_connected_cubes(start_cube,all_cubes):
                 neighbors[new_neighbor] = True
 
     return neighbors
+
 
 def main():
     # input parsing
@@ -78,16 +90,14 @@ def main():
 
     # get all air cubes in the cube space (including a padding of 1)
     air_cubes,min_x,max_x,min_y,max_y,min_z,max_z = get_all_air_cubes_in_cube_space(cubes)
-
     # define an edge air cube which is known to be outside of the cube structure
     edge__air_cube = (min_x-1,min_y-1,min_z-1)
-
+    
     # find all connected air cubes to this air cube
     external_air_cubes = find_all_connected_cubes(edge__air_cube,air_cubes)
     
     # count the number of external surfaces
     n_external_surfaces = count_external_surfaces(external_air_cubes,cubes)
-
     print(n_external_surfaces)
 
 cProfile.run('main()',sort='cumtime')
